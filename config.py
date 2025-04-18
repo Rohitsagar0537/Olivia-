5893fa46bfdd1963155ccc4e0c8a0223d30b46b5
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-04-18 10:55:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KING0712/QUEEN_MUSIC",
+    "https://github.com/Rohitsagar0537/Olivia-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
